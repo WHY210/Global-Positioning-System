96122df50dc216b3836c854e856b6fe25069e333
WHY <92018686+Dulcinea-WHY@users.noreply.github.com>
2022-06-09 19:49:18 +0800

    Add files via upload

--- HW6/HW6.py
+++ HW6/HW6.py
@@ -7,7 +7,8 @@ lon  = str(input("大地經度 λ (度 分 秒, E+/W-) = ")).split()
 h    = float(input("大地橢球高 h (m)= ") )
 lat = ((int(lat[0])*3600+int(lat[1])*60+int(lat[2]))/3600)*math.pi/180
 lon = ((int(lon[0])*3600+int(lon[1])*60+int(lon[2]))/3600)*math.pi/180
-geodetic = (lat, lon, h) 
+geodetic = [lat, lon, h]
+print("大地坐標 = ", geodetic)
 
 # GRS80橢球體 長半徑a(m) / 扁率f / 離心率e
 a = 6387137
@@ -21,5 +22,40 @@ N = a/(1-(e*math.sin(lat))**2)**0.5
 X = (N+h)*math.cos(lat)*math.cos(lon)
 Y = (N+h)*math.cos(lat)*math.sin(lon)
 Z = (N*(1-e**2)+h)*math.sin(lat)
+geocentric = [X,Y,Z]
+print("ANS: 地心地固坐標 = ", geocentric)
 
-print("ANS: ",X,Y,Z)
+
+
+
+## 地心地固坐標系統 (X,Y,Z) 疊代反算 大地坐標系統 (φ,λ,h) 
+
+D = (X**2+Y**2)**0.5
+lon = math.atan2(Y,X)
+
+#疊代
+h = 0
+for i in range(1000):
+    lat = math.atan(Z/(D*(1-e**2)))
+    N = a/(1-(e*math.sin(lat))**2)**0.5
+    h = D/math.cos(lat)-N
+    lat = math.atan(Z*(N+h)/(D*((1-e**2)*N+h)))
+    N = a/(1-(e*math.sin(lat))**2)**0.5
+    h = D/math.cos(lat)-N
+
+#所求 : 大地坐標系統 (φ,λ,h) -弧度 
+geodetic = [lat, lon, h]
+print("ANS: 大地坐標 (弧度) = ", geodetic)
+
+#所求 : 大地坐標系統 (φ,λ,h) -度分秒 
+lat = (lat*180/math.pi)%3600
+lat_degree = int(lat)
+lat_minute = int((lat-int(lat))*60)
+lat_second = int(((lat-int(lat))*60-int((lat-int(lat))*60))*60)
+lat = "%s°%s\'%s\"" %(lat_degree, lat_minute, lat_second)
+lon = (lon*180/math.pi)%3600
+lon_degree = int(lon)
+lon_minute = int((lon-int(lon))*60)
+lon_second = int(((lon-int(lon))*60-int((lon-int(lon))*60))*60)
+lon = "%s°%s\'%s\"" %(lon_degree, lon_minute, lon_second)
+print("             (度分秒) = [%s, %s, %s]" %(lat, lon, h))
